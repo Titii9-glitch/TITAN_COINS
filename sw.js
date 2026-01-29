bdaeed1de5b35db86afc0a7bdf63f43f69414fba
DAVID ORTIZ <ortizdavid571@gmail.com>
2026-01-29 13:38:56 -0500

    Correccion final de scripts

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10533946
+
+"domain": "3nbf4.com",
+
+"zoneId": 10533946
+
 }
+
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
